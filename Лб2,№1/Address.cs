3a4a9ea52c6ad833f2cf66c6f4855f0135ab9678
Денис Зайдун <denzaydunden@gmail.com>
2021-09-23 16:34:23 +0300

    first

---
+++ Лб2,№1/Address.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Лб2__1
+{
+    class Address
+    {
+        private int _index, _house, _apartment;
+        private string _country, _city, _street;
+
+        public int index
+        {
+            get
+            {
+                return _index;
+            }
+            set
+            {
+                _index = value;
+            }
+        }
+
+        public string country
+        {
+            get
+            {
+                return _country;
+            }
+            set
+            {
+                _country = value;
+            }
+        }
+
+        public string city
+        {
+            get
+            {
+                return _city;
+            }
+            set
+            {
+                _city = value;
+            }
+        }
+
+        public string street
+        {
+            get
+            {
+                return _street;
+            }
+            set
+            {
+                _street = value;
+            }
+        }
+
+        public int house
+        {
+            get
+            {
+                return _house;
+            }
+            set
+            {
+                _house = value;
+            }
+        }
+
+        public int apartment
+        {
+            get
+            {
+                return _apartment;
+            }
+            set
+            {
+                _apartment = value;
+            }
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Індекс: {_index}");
+            Console.WriteLine($"Країна: {_country}");
+            Console.WriteLine($"Місто: {_city}");
+            Console.WriteLine($"Вулиця: {_street}");
+            Console.WriteLine($"Будинок: {_house}");
+            Console.WriteLine($"Квартира: {_apartment}");
+        }
+    }
+}
---
+++ Лб2,№1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Лб2__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Console.InputEncoding = System.Text.Encoding.Unicode; //для правильного відображення укр.мови
+
+            Address Den = new Address();
+
+            Den.index = 71008;
+            Den.country = "Україна";
+            Den.city = "Київ";
+            Den.street = "вулиця Михайла Грушевського";
+            Den.house = 12;
+            Den.apartment = 522;
+
+            Den.GetInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
